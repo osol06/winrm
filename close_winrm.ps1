54171facf97e88ee1794d468012ff24961cc4dc3
sasaki.takuma <sasaki.takuma@fujitsu.com>
2020-03-15 20:00:42 +0900

    test

--- close_winrm.ps1
+++ close_winrm.ps1
@@ -24,40 +24,6 @@ winrm set winrm/config/service/auth '@{Basic="false"}'
 Write-Host "Removing firewall rule to allow WinRM HTTPS."
 Remove-NetFirewallRule -DisplayName "Allow WinRM HTTPS"
 
-# Initialize Proxy Server
-$del_proxy = Read-Host 'Is Proxy Server Information initialized ? This is only VIF. (y/n) [n] '
-if ( $del_proxy -eq 'y')
-{
-  $adm_uid    = whoami /user | Select-String 'Administrator'
-  $adm_uid    = (-split $adm_uid)[1]
-  $reg_key    = "Registry::HKEY_USERS\$adm_uid\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
-  foreach ( $entry in 'ProxyOverride', 'ProxyServer', 'ProxyEnable' )
-  {
-    if ( Test-Path $reg_key )
-    {
-      $retReg = (Get-ItemProperty $reg_key).$entry
-      if ( $retReg -ne $null )
-      {
-        Remove-ItemProperty -Path $reg_key -Name $entry
-        Write-Host "Initialize config in $entry : $retReg"
-      }
-      else
-      {
-        Write-Host "$entry is already initialized." -ForegroundColor Cyan
-      }
-    }
-    else
-    {
-      Write-Host "$reg_key is not exsit." -ForegroundColor Red
-    }
-  }
-  Write-Host "OK: Initialize proxy server setting." -ForegroundColor Green
-}
-else
-{
-  Write-Host "Not initialize proxy server setting by user."
-}
-
 Write-Host "Finalize tool is success." -ForegroundColor Green
 
 exit 0
